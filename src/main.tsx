400b800ae3cedde58ba4033e4567603ea123a79c
leandroquiroga <leandroquiroga9514@gmail.com>
2023-05-13 20:12:23 -0300

    feat: Se cambio el comoponente inicial de nuestra app App.tsx por MapBox.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx';
+import { MapBox } from './MapBox.tsx';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MapBox />
+  </React.StrictMode>
+);
