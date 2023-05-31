0d962f83fe1a3db690ac36ce97ba8e8d2fcc9bed
leandroquiroga <leandroquiroga9514@gmail.com>
2023-05-31 20:56:43 -0300

    feat: Se creo una funcion que segun el routingProfile retorna coordenadas diferentes

--- src/components/ButtonRoutingProfile.tsx
+++ src/components/ButtonRoutingProfile.tsx
@@ -1,21 +1,38 @@
+import { useContext } from "react";
 import { IconButton } from ".";
+import { MapsContext, PlacesContext } from "../context";
+import { directionService } from "../services/direction_services";
 
 
 export const ButtonRoutingProfile = (): JSX.Element => {
-  //TODO: Utilizar contexto de la aplicacion para poder manejar los valores de cada boton
-  const arrRoutingProfile = ["auto", "caminando", "bicicleta",]
+  const arrRoutingProfile = ["driving", "walking", "cycling"];
+  const { placeCurrent, setRoutingProfile,  createPolyline } = useContext(MapsContext);
+  const { userLocation } = useContext(PlacesContext);
+
+  // Retorna la data de las coordenadas entre dos puntos dependiendo del routing profile
+  const handleRoute = async (route: string) => {
+    const [start, end] = placeCurrent;
+    const response = await directionService(route, userLocation!, [start, end]);
+    const { geometry } = response.routes[0];
+    const { coordinates } = geometry;
+    setRoutingProfile(route);
+    createPolyline(coordinates);
+  };
 
   return (
-    <section className="input-customer-radio">
+    <section>
       {arrRoutingProfile.map((route, index) => (
         <button
           value={route}
-          className="btn btn-light btn-sm my-1 rounded-3"
+          className="btn btn-primary btn-sm my-1 mx-2 rounded-3"
           key={index}
-        >
+          onClick={() => {
+            console.log(route)
+            handleRoute(route);
+          }}>
           <IconButton routeProfile={route} />
         </button>
       ))}
     </section>
   );
-}
+};
