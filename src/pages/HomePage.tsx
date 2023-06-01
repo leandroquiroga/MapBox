3e88ea781f031e56133cf1a25d2efcea53fada3f
leandroquiroga <leandroquiroga9514@gmail.com>
2023-05-31 21:05:10 -0300

    fix: Se elimino la condicion y se modifico la estructura del componente, mejorar con lazyload

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,11 @@
-import { useContext } from "react";
 import { Layaout} from "../components";
 import { Footer } from "./Footer";
-import { PlacesContext } from "../context";
 
 export const HomePage = (): JSX.Element => {
-
-  const { showFooter } = useContext(PlacesContext);
   return (
     <section>
-        <Layaout />
-      {(showFooter) && <Footer />}
+      <Layaout />
+      <Footer />
     </section>
   );
 }
